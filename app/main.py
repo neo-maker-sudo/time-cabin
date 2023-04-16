351932750b0b1d84d0ee619132b94ec2cb1d7b94
neo <eyywqkgb@gmail.com>
2023-04-17 00:31:42 +0800

    feat(app/main.py,-route/video.py): added video route into application, and tortoise-orm register settings

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from app.config import setting
+from app.routes import video
 
 app = FastAPI()
+
+app.include_router(video.router)
+
+register_tortoise(
+    app,
+    db_url=setting.db_url,
+    modules={"models": setting.db_models},
+)
 
 @app.get("/")
 def home_page_view():
