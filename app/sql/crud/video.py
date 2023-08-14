ce943c73e76c91cf7877eb0899cf8c0dd4c0353c
Neo <eyywqkgb@gmail.com>
2023-08-14 22:12:26 +0800

    feat(crud): add transaction for whole create, update, delete crud function

--- app/sql/crud/video.py
+++ app/sql/crud/video.py
@@ -1,4 +1,5 @@
 from tortoise.exceptions import DoesNotExist
+from tortoise.transactions import atomic
 from app.exceptions.videos import VideoDoesNotExistException
 from app.sql.models.video import video_pydantic, Videos
 
@@ -17,6 +18,7 @@ async def retrieve_videos():
     return await video_pydantic.from_queryset(Videos.all())
 
 
+@atomic()
 async def create_video(create_object: dict):
     try:
         video = await Videos.create(**create_object)
@@ -28,6 +30,7 @@ async def create_video(create_object: dict):
     return await video_pydantic.from_tortoise_orm(video)
 
 
+@atomic()
 async def update_video(video_id: int, update_object: dict):
     try:
         await Videos.filter(id=video_id).update(**update_object)
@@ -38,6 +41,7 @@ async def update_video(video_id: int, update_object: dict):
     return await video_pydantic.from_queryset_single(Videos.get(id=video_id))
 
 
+@atomic()
 async def delete_video(video_id: int) -> None:
     try:
         deleted_count= await Videos.filter(id=video_id).delete()
